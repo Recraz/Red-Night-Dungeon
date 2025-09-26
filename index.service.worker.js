5c14001f998f3848c01b2b3515f2c8e462980b2b
Recraz <95542186+Recraz@users.noreply.github.com>
2025-09-26 03:38:31 +0200

    0.1.1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758850120|857519853';
+const CACHE_VERSION = '1758850688|1425386546';
 /** @type {string} */
 const CACHE_PREFIX = 'Red Night Dungeo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
